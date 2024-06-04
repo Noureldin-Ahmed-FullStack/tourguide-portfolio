75116bb9ff1e75fa3578c95986b1b7d0bc53c9a2
Noureldin-Ahmed-FullStack <noureldin2662002@gmail.com>
2024-06-04 04:10:34 +0300

    changed Image display to dialogue

--- src/Components/Pages/Gallery.jsx
+++ src/Components/Pages/Gallery.jsx
@@ -1,79 +1,91 @@
-import { IconButton, ImageList, ImageListItem, ImageListItemBar, ListSubheader, useMediaQuery, useTheme } from '@mui/material';
-import React, { useState } from 'react'
-import ImageLoaderSkeleton from '../ImageLoaderSkeleton';
-import Modal from '../Modal Stuff/Modal';
-import { AnimatePresence } from 'framer-motion';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  ImageList,
+  ImageListItem,
+  ImageListItemBar,
+  ListSubheader,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import React, { useState } from "react";
+import ImageLoaderSkeleton from "../ImageLoaderSkeleton";
 export default function Gallery() {
   const itemData = [
     {
-      img: '1.jpg',
-      title: 'Coffee',
-      author: '@nolanissac',
+      img: "1.jpg",
+      title: "Coffee",
+      author: "@nolanissac",
       cols: 2,
     },
     {
-      img: '2.jpg',
-      title: 'Hats',
-      author: '@hjrc33',
+      img: "2.jpg",
+      title: "Hats",
+      author: "@hjrc33",
       cols: 2,
     },
     {
-      img: '3.jpg',
-      title: 'Honey',
-      author: '@arwinneil',
+      img: "3.jpg",
+      title: "Honey",
+      author: "@arwinneil",
       rows: 2,
       cols: 2,
       featured: true,
     },
     {
-      img: '4.jpg',
-      title: 'Basketball',
-      author: '@tjdragotta',
+      img: "4.jpg",
+      title: "Basketball",
+      author: "@tjdragotta",
     },
     {
-      img: '5.jpg',
-      title: 'Fern',
-      author: '@katie_wasserman',
+      img: "5.jpg",
+      title: "Fern",
+      author: "@katie_wasserman",
     },
     {
-      img: '6.jpg',
-      title: 'Mushrooms',
-      author: '@silverdalex',
+      img: "6.jpg",
+      title: "Mushrooms",
+      author: "@silverdalex",
       rows: 2,
       cols: 2,
     },
     {
-      img: '7.jpg',
-      title: 'Tomato basil',
-      author: '@shelleypauls',
+      img: "7.jpg",
+      title: "Tomato basil",
+      author: "@shelleypauls",
     },
     {
-      img: '8.jpg',
-      title: 'Sea star',
-      author: '@peterlaster',
+      img: "8.jpg",
+      title: "Sea star",
+      author: "@peterlaster",
     },
     {
-      img: '9.jpg',
-      title: 'Bike',
-      author: '@southside_customs',
+      img: "9.jpg",
+      title: "Bike",
+      author: "@southside_customs",
       cols: 2,
     },
     {
-      img: '10.jpg',
-      title: 'Bike',
-      author: '@southside_customs',
+      img: "10.jpg",
+      title: "Bike",
+      author: "@southside_customs",
       cols: 2,
     },
     {
-      img: '11.jpg',
-      title: 'Bike',
-      author: '@southside_customs',
+      img: "11.jpg",
+      title: "Bike",
+      author: "@southside_customs",
       cols: 2,
     },
     {
-      img: '12.jpg',
-      title: 'Bike',
-      author: '@southside_customs',
+      img: "12.jpg",
+      title: "Bike",
+      author: "@southside_customs",
       cols: 3,
     },
   ];
@@ -82,14 +94,17 @@ export default function Gallery() {
   const [SelectedImage, setSelectedImage] = useState(false);
   const close = () => setModalOpen(false);
   const open = () => setModalOpen(true);
-  const displayImage = (image)=>{
-    setSelectedImage(image)
-    modalOpen? close(): open()
-  }
-  const isXs = useMediaQuery(theme.breakpoints.down('xs'));
-  const isSm = useMediaQuery(theme.breakpoints.down('sm'));
-  const isMd = useMediaQuery(theme.breakpoints.down('md'));
-  const isLg = useMediaQuery(theme.breakpoints.down('lg'));
+  const displayImage = (image) => {
+    setSelectedImage(image);
+    modalOpen ? close() : open();
+  };
+  const handleClose = () => {
+    close();
+  };
+  const isXs = useMediaQuery(theme.breakpoints.down("xs"));
+  const isSm = useMediaQuery(theme.breakpoints.down("sm"));
+  const isMd = useMediaQuery(theme.breakpoints.down("md"));
+  const isLg = useMediaQuery(theme.breakpoints.down("lg"));
   let cols = 4; // default
 
   if (isXs) {
@@ -102,28 +117,69 @@ export default function Gallery() {
     cols = 4;
   }
   return (
-    <div className='container galleryMT'>
-      <AnimatePresence
-        initial={false}
-        mode='wait'
-        onExitComplete={() => null}
+    <div className="container galleryMT">
+      <Dialog
+        sx={{
+          "& .MuiPaper-root": {
+            backgroundColor: "transparent", // Change to your desired color
+          },
+        }}
+        fullWidth={true}
+        maxWidth={'lg'}
+        open={modalOpen}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
       >
-        {modalOpen && <Modal modalOpen={modalOpen} animation={"dropIn"} handleClose={close} >
-          <img className='w-100 rounded-2 vids' src={`https://ssniper.sirv.com/TourguideProject/Gallery/${SelectedImage}`} />
-        </Modal>}
-      </AnimatePresence>
-      <ImageList cols={cols} gap={30} variant='masonry' sx={{ overflow: 'hidden' }}>
+        <DialogActions sx={{padding:'0'}}>
+          <Button sx={{padding:'0'}} onClick={handleClose} >
+            <CloseIcon
+              sx={{
+                fontSize:'3rem',
+                color: "white", // Changes cursor to pointer to indicate it's clickable
+                transition:'all 0.6s',
+                borderRadius:'0.3rem',
+                "&:hover": {
+                  color: "black", // Change the color on hover
+                  backgroundColor:'white',
+                  transform: "scale(1.1)", // Scale up slightly on hover
+                },
+              }}
+              onClick={handleClose}
+            />
+          </Button>
+        </DialogActions>
+        <DialogContent sx={{padding:'0'}}>
+          <div className="bg-transparent d-flex justify-content-center">
+            <img
+              className="rounded-2 DialogMedia"
+              src={`https://ssniper.sirv.com/TourguideProject/Gallery/${SelectedImage}`}
+            />
+          </div>
+        </DialogContent>
+      </Dialog>
 
+      <ImageList
+        cols={cols}
+        gap={30}
+        variant="masonry"
+        sx={{ overflow: "hidden" }}
+      >
         {itemData.map((item) => (
-          <div key={item.img} onClick={()=>displayImage(item.img)}>
-            <ImageListItem className='scaleOnHover'>
-              <ImageLoaderSkeleton modalOpen={modalOpen} height={282} src={`https://ssniper.sirv.com/TourguideProject/Gallery/${item.img}`} title={item.title} />
+          <div key={item.img} onClick={() => displayImage(item.img)}>
+            <ImageListItem className="scaleOnHover">
+              <ImageLoaderSkeleton
+                modalOpen={modalOpen}
+                height={282}
+                src={`https://ssniper.sirv.com/TourguideProject/Gallery/${item.img}`}
+                title={item.title}
+              />
               <ImageListItemBar
                 title={item.title}
                 subtitle={item.author}
                 actionIcon={
                   <IconButton
-                    sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                    sx={{ color: "rgba(255, 255, 255, 0.54)" }}
                     aria-label={`info about ${item.title}`}
                   >
                     {/* <InfoIcon /> */}
@@ -132,7 +188,6 @@ export default function Gallery() {
               />
             </ImageListItem>
           </div>
-
         ))}
       </ImageList>
     </div>
--- src/Components/Pages/ToursAll.jsx
+++ src/Components/Pages/ToursAll.jsx
@@ -1,119 +1,186 @@
-import { IconButton, ImageList, ImageListItem, ImageListItemBar, ListSubheader, useMediaQuery, useTheme } from '@mui/material';
-import React from 'react'
-import ImageLoaderSkeleton from '../ImageLoaderSkeleton';
+import {} from "@mui/material";
+import React, { useState } from "react";
+import {
+  useTheme,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  ImageList,
+  ImageListItem,
+  ImageListItemBar,
+  ListSubheader,
+  useMediaQuery,
+} from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import ImageLoaderSkeleton from "../ImageLoaderSkeleton";
 
 export default function ToursAll() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [SelectedImage, setSelectedImage] = useState(false);
+  const close = () => setModalOpen(false);
+  const open = () => setModalOpen(true);
+  const displayImage = (image) => {
+    setSelectedImage(image);
+    modalOpen ? close() : open();
+  };
+  const handleClose = () => {
+    close();
+  };
   const itemData = [
     {
-      img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-      title: 'Breakfast',
-      author: '@bkristastucchio',
+      img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+      title: "Breakfast",
+      author: "@bkristastucchio",
       rows: 2,
       cols: 2,
       featured: true,
     },
     {
-      img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-      title: 'Burger',
-      author: '@rollelflex_graphy726',
+      img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+      title: "Burger",
+      author: "@rollelflex_graphy726",
     },
     {
-      img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-      title: 'Camera',
-      author: '@helloimnik',
+      img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+      title: "Camera",
+      author: "@helloimnik",
     },
     {
-      img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-      title: 'Coffee',
-      author: '@nolanissac',
+      img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+      title: "Coffee",
+      author: "@nolanissac",
       cols: 2,
     },
     {
-      img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-      title: 'Hats',
-      author: '@hjrc33',
+      img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+      title: "Hats",
+      author: "@hjrc33",
       cols: 2,
     },
     {
-      img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-      title: 'Honey',
-      author: '@arwinneil',
+      img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+      title: "Honey",
+      author: "@arwinneil",
       rows: 2,
       cols: 2,
       featured: true,
     },
     {
-      img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-      title: 'Basketball',
-      author: '@tjdragotta',
+      img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
+      title: "Basketball",
+      author: "@tjdragotta",
     },
     {
-      img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-      title: 'Fern',
-      author: '@katie_wasserman',
+      img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
+      title: "Fern",
+      author: "@katie_wasserman",
     },
     {
-      img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-      title: 'Mushrooms',
-      author: '@silverdalex',
+      img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
+      title: "Mushrooms",
+      author: "@silverdalex",
       rows: 2,
       cols: 2,
     },
     {
-      img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-      title: 'Tomato basil',
-      author: '@shelleypauls',
+      img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
+      title: "Tomato basil",
+      author: "@shelleypauls",
     },
     {
-      img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-      title: 'Sea star',
-      author: '@peterlaster',
+      img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
+      title: "Sea star",
+      author: "@peterlaster",
     },
     {
-      img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-      title: 'Bike',
-      author: '@southside_customs',
+      img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
+      title: "Bike",
+      author: "@southside_customs",
       cols: 2,
     },
   ];
   const theme = useTheme();
-  const isXs = useMediaQuery(theme.breakpoints.down('xs'));
-  const isSm = useMediaQuery(theme.breakpoints.down('sm'));
-  const isMd = useMediaQuery(theme.breakpoints.down('md'));
-  const isLg = useMediaQuery(theme.breakpoints.down('lg'));
+  const isXs = useMediaQuery(theme.breakpoints.down("xs"));
+  const isSm = useMediaQuery(theme.breakpoints.down("sm"));
+  const isMd = useMediaQuery(theme.breakpoints.down("md"));
+  const isLg = useMediaQuery(theme.breakpoints.down("lg"));
   let cols = 4; // default
 
   if (isXs) {
-      cols = 1;
+    cols = 1;
   } else if (isSm) {
-      cols = 2;
+    cols = 2;
   } else if (isMd) {
-      cols = 3;
+    cols = 3;
   } else if (isLg) {
-      cols = 4;
+    cols = 4;
   }
   return (
-    <div className='container galleryMT'>
-      <ImageList cols={cols} gap={8} variant='masonry'>
-
+    <div className="container galleryMT">
+      <Dialog
+        sx={{
+          "& .MuiPaper-root": {
+            backgroundColor: "transparent", // Change to your desired color
+          },
+        }}
+        fullWidth={true}
+        maxWidth={"lg"}
+        open={modalOpen}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogActions sx={{ padding: "0" }}>
+          <Button sx={{ padding: "0" }} onClick={handleClose}>
+            <CloseIcon
+              sx={{
+                fontSize: "3rem",
+                color: "white", // Changes cursor to pointer to indicate it's clickable
+                transition: "all 0.6s",
+                borderRadius: "0.3rem",
+                "&:hover": {
+                  color: "black", // Change the color on hover
+                  backgroundColor: "white",
+                  transform: "scale(1.1)", // Scale up slightly on hover
+                },
+              }}
+              onClick={handleClose}
+            />
+          </Button>
+        </DialogActions>
+        <DialogContent sx={{ padding: "0" }}>
+          <div className="bg-transparent d-flex justify-content-center">
+            <img
+              className="rounded-2 DialogMedia"
+              src={`${SelectedImage}`}
+            />
+          </div>
+        </DialogContent>
+      </Dialog>
+      <ImageList cols={cols} gap={8} variant="masonry">
         {itemData.map((item) => (
-          <ImageListItem key={item.img}>
+          <div key={item.img} onClick={() => displayImage(item.img)}>
+          <ImageListItem >
             {/* <img
               srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
               src={`${item.img}?w=248&fit=crop&auto=format`}
               alt={item.title}
               loading="lazy"
             /> */}
-            <ImageLoaderSkeleton 
-            height='120px'
-            title='item.title'
-            src={`${item.img}?w=248&fit=crop&auto=format`}/>
+            <ImageLoaderSkeleton
+              height="120px"
+              title="item.title"
+              src={`${item.img}?w=248&fit=crop&auto=format`}
+            />
             <ImageListItemBar
               title={item.title}
               subtitle={item.author}
               actionIcon={
                 <IconButton
-                  sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                  sx={{ color: "rgba(255, 255, 255, 0.54)" }}
                   aria-label={`info about ${item.title}`}
                 >
                   {/* <InfoIcon /> */}
@@ -121,10 +188,9 @@ export default function ToursAll() {
               }
             />
           </ImageListItem>
+          </div>
         ))}
       </ImageList>
     </div>
   );
 }
-
-
